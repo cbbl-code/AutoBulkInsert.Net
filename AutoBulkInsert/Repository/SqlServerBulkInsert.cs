40a4ecef8dcdbf70f30f4c3c6f55db6595440543
cbbl-code <cbblimited@outlook.com>
2021-03-13 01:14:37 +0000

    Initial Commit

---
+++ AutoBulkInsert/Repository/SqlServerBulkInsert.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class Class1
+    {
+
+        public async Task BulkInsertAsync<T>(IEnumerable<T> insertCollection, SqlConnection sqlConnection)
+        {
+            SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(sqlConnection);
+            await sqlConnection.OpenAsync();
+            await ExecuteInsertAsync(sqlBulkCopy, sqlConnection,(IDataReader)null);
+        }
+
+        private async Task ExecuteInsertAsync(SqlBulkCopy sqlBulkCopy, 
+            SqlConnection sqlConnection,
+            IDataReader dataReader)
+        {
+            await sqlConnection.OpenAsync();
+            await sqlBulkCopy.WriteToServerAsync(dataReader);
+        }
+
+    }
+}
